0aaf63e29ae1fca21bab9f42ca3949484f569ef8
Noémie Lefrancq <50409293+noemielefrancq@users.noreply.github.com>
2022-01-28 16:13:27 +0000

    Comment code

--- Geographical_spread/Relative_risk.R
+++ Geographical_spread/Relative_risk.R
@@ -1,17 +1,29 @@
-########################################################################################
-## Relative risk of pairs of sequences to come from the same location versus different locations
-########################################################################################
+#######################################################
+## Figure 2: Relative risk of coming from the 
+##  same location, vs. different location,
+##    when separated from a specific 
+##           genetic distance
+#######################################################
+### Author: Noemie Lefrancq
+### Last modification: 28/01/2022
+#######################################################
+
+
+## Load necessary packages
 library(ape)
 library(stringr)
 library(lubridate)
 library(REdaS)
 library(questionr)
 
-##Load the data--------------------------------------------------------------------------------------
-##Tree
+
+
+## Load trees
 trees= readRDS('Data/Beast_10_posterior_trees_26012022.rds')
 
-##Create the global master dataframes and lists------------------------------------------------------
+
+
+## Create the global master dataframes
 case.data_all = read.csv(file = 'Data/Metadata_analyses_26012022.csv', sep = ';')
 colnames(case.data_all)[2] = 'Isolates'
 tree_tmp = trees[[1]]
@@ -20,18 +32,21 @@ name.isolates = sapply(tree_tmp$tip.label, function(x)strsplit(x,split="_")[[1]]
 master.dat = case.data_all[match(name.isolates, case.data_all$Isolates),]
 master.seq.names = master.dat$Isolates
 
-##Compute matrices ----------------------------------------------------------------------------------
 
-########################  TIME  ####################################
-#Compute time distances
+######################################################################
+## Compute matrices 
+######################################################################
+
+##  Time differences between isolates
 time_mat = abs(outer(master.dat$Collection.Year,master.dat$Collection.Year,"-"))
 colnames(time_mat) = master.seq.names
 rownames(time_mat) = master.seq.names
 diag(time_mat)<-NA
-####################################################################
 
-######################## GENETIC ###################################
-##Store genetic distance matrices into a huge matrix################
+
+## Genetic distances between isolates 
+# Store genetic distances into a list, 
+# which corresponds to each tree of the posterior distribution
 nsim = 10
 ntrees = length(trees)
 MRCA.mats<-array(NaN,c(length(master.seq.names),length(master.seq.names),nsim))
@@ -50,17 +65,17 @@ for (ii in 1:nsim){
   rownames(gene_mat) = master.seq.names
   diag(gene_mat)<-NA
   
-  MRCA_mat = (gene_mat - time_mat)/2 #make the genetic matrix "independant" of the difference in the isolation dates 
+  MRCA_mat = (gene_mat - time_mat)/2
   MRCA_mat[which(MRCA_mat == 0)] = 1E-6
   
   MRCA.mats[,,ii]<- MRCA_mat
   print(paste0(ii, ' / ', nsim))
 }
 rm(trees)
-######################################################################
 
-######################## GEOGRAPHY ###################################
-#Matrix region, world
+
+
+## Matrix same region  (1:pair from the same region, 0: pair NOT from the same region)
 master.dat_tmp = master.dat
 geo_mat_region_world<-matrix(0,length(master.dat_tmp$Country),length(master.dat_tmp$Country))
 master.dat_tmp$Region = as.factor(master.dat_tmp$Region)
@@ -72,7 +87,9 @@ colnames(geo_mat_region_world) = master.seq.names
 rownames(geo_mat_region_world) = master.seq.names
 diag(geo_mat_region_world)<-NA
 
-#Matrix States in the US
+
+
+## Matrix same US state  (1:pair from the same region, 0: pair NOT from the same region)
 a = length(which(master.dat$Country=='US'))
 master.dat_tmp = master.dat[which(master.dat$Country=='US'),]
 geo_mat_states_US<-matrix(0,a,a)
@@ -85,7 +102,9 @@ colnames(geo_mat_states_US) = master.seq.names[which(master.dat$Country=='US')]
 rownames(geo_mat_states_US) = master.seq.names[which(master.dat$Country=='US')]
 diag(geo_mat_states_US)<-NA
 
-#Matrix countries
+
+
+## Matrix same countries (1:pair from the same country, 0: pair NOT from the same country)
 geo_mat_country<-matrix(0,length(master.dat$Country),length(master.dat$Country))
 master.dat$Country = as.factor(master.dat$Country)
 for (i in levels(master.dat$Country)){
@@ -96,7 +115,8 @@ colnames(geo_mat_country) = master.seq.names
 rownames(geo_mat_country) = master.seq.names
 diag(geo_mat_country)<-NA
 
-#Matrix continent
+
+## Matrix same continent (1:pair from the same continent, 0: pair NOT from the same continent)
 geo_mat_continent<-matrix(0,length(master.dat$Continent),length(master.dat$Continent))
 master.dat$Continent = as.factor(master.dat$Continent)
 for (i in levels(master.dat$Continent)){
@@ -112,19 +132,8 @@ diag(geo_mat_continent)<-NA
 
 
 ######################################################################
-## Compute the relative risks                                        #   
+## Main function: compute the relative risks
 ######################################################################
-### Parameters for the RR plots, in "continuous" MRCA intervals
-# Time to MRCA from 0 to 10 years
-Pmax = seq(1, 20, 0.1)
-windows = 1.5
-Pmin = Pmax - windows
-Pmin[which(Pmin<0)]<-0
-pmid1<-(Pmin+Pmax)/2
-
-nboot = 10
-
-## Function to compute the RR
 ratio_bootstrap <- function(x, geo_mat, time_mat2, MRCA_mat){
   geo_mat.tmp = geo_mat[x,x]
   time_mat.tmp = time_mat2[x,x]
@@ -152,17 +161,36 @@ ratio_bootstrap <- function(x, geo_mat, time_mat2, MRCA_mat){
   
   return(rr[-length(rr)])
 }
+######################################################################
 
-###########################################################################################################################################################
-## 1 : Continuous Plot RR in Europe by country
-###########################################################################################################################################################
-# Create relative risk matrix
+
+
+
+
+######################################################################
+## Main computation: compute the relative risks
+######################################################################
+## Genetic  distance windows to consider (in years)
+Pmax = seq(1, 20, 0.1)
+windows = 1.5
+Pmin = Pmax - windows
+Pmin[which(Pmin<0)]<-0
+pmid1<-(Pmin+Pmax)/2
+
+## Number of bootstrap iterations
+nboot = 10
+
+
+
+## 1 : RR in Europe by country #######################################
+## Create matrix to store results
 boot.out = matrix(NA, length(pmid1), nsim*nboot)
 
 ## Data on which we are computing the RR
 A = c(which(master.dat$Dataset == 'Pasteur_Brisse+EU'), which(master.dat$Dataset == 'EU_dataset'))
 nseq = length(A)
-##Bootstrap to create the ci
+
+## Compute relative risk, for each bootstrap iteration
 for(j in 1:nsim){
   print(paste0('nsim : ', j, '/', nsim))
   geo_mat = geo_mat_country[A,A]
@@ -176,26 +204,30 @@ for(j in 1:nsim){
   }
 }
 
-##Compute the ci over the 2 to nboot +1 columns 
-boot.ci1_EU = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.025, 0.975), na.rm = T)
-boot.ci.m_EU = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.5), na.rm = T)
+## Compute the confidence intervals
+boot.ci1_EU = apply(boot.out, 1, quantile, probs = c(0.025, 0.975), na.rm = T)
+boot.ci.m_EU = apply(boot.out, 1, quantile, probs = c(0.5), na.rm = T)
 
+## Store parameters
 list  = list('pmid' = pmid1, 
              'median' = boot.ci.m_EU,
              'ci' = boot.ci1_EU)
+
+## Save results
 saveRDS(list, 'Relative_risk/RR_Pertussis_country_europe.rds')
 
-###########################################################################################################################################################
-## 2 : Continuous Plot RR in USA by state
-###########################################################################################################################################################
-# Create relative risk matrix
+
+
+## 2 : RR in USA by state ############################################
+## Create matrix to store results
 boot.out = matrix(NA, length(pmid1), nsim*nboot)
 
 ## Data on which we are computing the RR
 A = which(master.dat$Country == 'US' & master.dat$Collection.Year >= 2010)
 nseq = length(A)
 B = which(master.dat[which(master.dat$Country == "US"),]$Collection.Year>= 2010)
-##Bootstrap to create the ci
+
+## Compute relative risk, for each bootstrap iteration
 for(j in 1:nsim){
   print(paste0('nsim : ', j, '/', nsim))
   geo_mat = geo_mat_states_US[B,B]
@@ -209,19 +241,22 @@ for(j in 1:nsim){
   }
 }
 
-##Compute the ci over the 2 to nboot +1 columns
-boot.ci1_US = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.025, 0.975), na.rm = T)
-boot.ci.m_US = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.5), na.rm = T)
+## Compute the confidence intervals
+boot.ci1_US = apply(boot.out, 1, quantile, probs = c(0.025, 0.975), na.rm = T)
+boot.ci.m_US = apply(boot.out, 1, quantile, probs = c(0.5), na.rm = T)
 
+## Store parameters
 list  = list('pmid' = pmid1, 
              'median' = boot.ci.m_US,
              'ci' = boot.ci1_US)
+
+## Save results
 saveRDS(list, 'Relative_risk/RR_Pertussis_US_states.rds')
 
-###########################################################################################################################################################
-## 3 : Plot RR between continents (average across different continents)
-###########################################################################################################################################################
-# Create relative risk matrix
+
+
+## 3 : RR between continents (average across different continents) #####
+## Create matrix to store results
 boot.out = matrix(NA, length(pmid1), nsim*nboot)
 
 ## Data on which we are computing the RR
@@ -230,7 +265,7 @@ nseq = length(A)
 nb_seq_per_continent = 300
 continents = c(levels(as.factor(master.dat$Continent)))[-1] ## Remove Africa:  not enough sequences
 
-##Bootstrap to create the ci
+## Compute relative risk, for each bootstrap iteration
 for(j in 1:nsim){
   print(paste0('nsim : ', j, '/', nsim))
   geo_mat = geo_mat_continent[A,A]
@@ -238,26 +273,28 @@ for(j in 1:nsim){
   MRCA_mat = MRCA.mats[,,j]
   MRCA_mat2 = MRCA_mat[A,A]
   for (i in (1:(nboot))){
-    ## resample isolates, making sure to have equal representation of all continents
+    ## Resample isolates, allowing for even sampling by location
     tmp = NULL
     for(k in 1:length(continents)){
       b = which(master.dat$Continent[which(master.dat$Collection.Year>=2010)] == continents[k])
       tmp = c(tmp, sample(b, nb_seq_per_continent, replace = T))
     }
-    # tmp = sample(nseq, nseq, replace = T) ## old
     rr = ratio_bootstrap(tmp, geo_mat, time_mat2, MRCA_mat2)
     boot.out[,(j-1)*nboot + i] = rr
   }
 }
 
-##Compute the ci over the 2 to nboot +1 columns 
+## Compute the confidence intervals
 boot.ci1_Continents = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.025, 0.975), na.rm = T)
 boot.ci.m_Continents = apply(boot.out[,(2:(nboot+1))], 1, quantile, probs = c(0.5), na.rm = T)
 
+
+## Store parameters
 list  = list('pmid' = pmid1, 
              'median' = boot.ci.m_Continents,
              'ci' = boot.ci1_Continents)
 
+## Save results
 saveRDS(list, 'Relative_risk/RR_Pertussis_average_continents.rds')
 #####################################################################################
 
@@ -265,10 +302,9 @@ saveRDS(list, 'Relative_risk/RR_Pertussis_average_continents.rds')
 
 
 
-
-###########################################################################################################################################################
-## Plot RR
-###########################################################################################################################################################
+####################################################################################
+## Plot results
+####################################################################################
 US_states = readRDS('Relative_risk/RR_Pertussis_US_states.rds')
 EU_countries = readRDS('Relative_risk/RR_Pertussis_country_europe.rds')
 Continents = readRDS('Relative_risk/RR_Pertussis_average_continents.rds')
@@ -312,6 +348,4 @@ axis(2, las = 2, at = c(0.25, 1, 10, 100, 1000), labels = c(0.25, 1, 10, 100, '>
 boot.ci1_compiled[[i]][which(boot.ci1_compiled[[i]] == 0)] = 0.001
 polygon(x = c(pmid1, rev(pmid1)), y = c(boot.ci1_compiled[[i]][1,], rev(boot.ci1_compiled[[i]][2,])), 
         col = adjustcolor(color[i], alpha.f = 0.5), border = F)
-
 ###########################################################################################################################################################
-
